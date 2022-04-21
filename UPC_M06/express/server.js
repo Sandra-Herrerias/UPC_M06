7af8297f24998807ea0849b0cc1b9a6dbbc22036
Jordi_Rocha <jordi.rocha@pue.es>
2022-04-21 12:14:12 +0200

    Express + Angular

---
+++ UPC_M06/express/server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.delete('/deleteProduct/:id', function (req, res) {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        database: 'm06_pt2',
+        user: 'root',
+        password: ''
+    });
+    connection.connect(function (err) {
+        if (err) {
+            console.error('Error connecting: ' + err.stack);
+            return;
+        }
+    });
+
+    connection.query('DELETE FROM products WHERE id = ?', [req.params.id], function (error, results, field) {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+        console.log(results);
+    });
+
+    connection.end();
+})
+
+app.get('/getMangas', function (req, res) {
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        database: 'm06_pt2',
+        user: 'root',
+        password: ''
+    });
+    connection.connect(function (err) {
+        if (err) {
+            console.error('Error connecting: ' + err.stack);
+            return;
+        }
+        // console.log('Connected as id ' + connection.threadId);
+    });
+
+    connection.query('select * from products where type like ?', ["manga"], function (error, results, field) {
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+        console.log('enviado!')
+    });
+
+    // jsonData.data.forEach(function (manga) {
+    //     connection.query('insert into products values (null,?,?,?,?,?,?,?)', [manga.attributes.canonicalTitle, 
+    //         ,manga.type, manga.attributes.synopsis, , 
+    //         manga.attributes.posterImage.large, Math.random() * (23 - 6) + 6 ] ,function (error, results, field) {
+    //         if (error) throw error;
+    //         // res.send(JSON.stringify(results));
+    //         console.log('insertado')
+    //     });
+    // })
+
+
+    connection.end();
+})
+
+app.put('/updateManga', function (req, res) {
+    console.log(req.body.params.id);
+
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        database: 'm06_pt2',
+        user: 'root',
+        password: ''
+    });
+    connection.connect(function (err) {
+        if (err) {
+            console.error('Error connecting: ' + err.stack);
+            return;
+        }
+        // console.log('Connected as id ' + connection.threadId);
+    });
+
+    connection.query('update products set price = ?, releaseDate=?,synopsis=?,title=?,type=? WHERE id = ? ',
+        [req.body.params.price, req.body.params.releaseDate, req.body.params.synopsis, req.body.params.title, req.body.params.type,req.body.params.id], function (error, results, field) {
+            if (error) throw error;
+            res.send(JSON.stringify(results));
+            console.log('updated!')
+        });
+    connection.end();
+})
+
+app.listen(3000);
