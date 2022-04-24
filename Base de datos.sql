9e9774e8af59298016834f00bec319b347a2f9c3
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-04-24 13:55:03 +0200

    modificació del script de bbdd + server.js

--- Base de datos.sql
+++ Base de datos.sql
@@ -7,6 +7,9 @@
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
+DROP DATABASE IF EXISTS upc_explodingkittens;
+CREATE DATABASE upc_explodingkittens;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
